ffc3fba9f201b823480f27cdb82a7ff97a71b3c4
CYWX1519 <sujunjunsjsj@gmail.com>
2022-11-28 12:31:27 +0800

    add web page redirect

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as router from 'router'
 
 const app = express()
 
@@ -7,11 +8,15 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 app.use(bodyParser.text({ type: '*/*' }))
 app.use(express.static('public'))
+app.use('router', router)
 
 app.get('/api/legado', require('./api/legado'))
 app.post('/api/ra', require('./api/ra'))
 app.get('/api/azure', require('./api/azure'))
 app.post('/api/azure', require('./api/azure'))
+app.get('*', function (req, res) {
+  res.redirect('/')
+})
 app.listen(port, () => {
   console.info(`应用正在监听 ${port} 端口`)
 })
